c0dfab18177dc670522b839b6a4612c955ef5324
roneydev01 <roneyfreitas.rf@gmail.com>
2020-10-08 00:21:16 -0300

    Criando Helper ambiente

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
   def data_br(data_us)
     data_us.strftime("%d/%m/%Y")
   end
+
+  def ambiente
+    if Rails.env.development? 
+      "Desenvolvimento"
+    elsif Reils.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
