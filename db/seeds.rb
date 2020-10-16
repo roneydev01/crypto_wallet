72bd320875bb9c31c8d1a30861ef6bc543d96520
roneydev01 <roneyfreitas.rf@gmail.com>
2020-10-16 11:47:55 -0300

    Refatorendo o seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Criando Moedas..."
 
-Coin.create!(
-  description: "Bitcoin",
-  acronym: "BTC",
-  url_image:"https://upload.wikimedia.org/wikipedia/commons/c/cf/Bitcoin.com_logo.png"
+spinner = TTY::Spinner.new("[:spinner] Criando Moedas...", format: :pulse_2)
+spinner.auto_spin
+
+coins = (
+  [
+    {
+      description: "Bitcoin",
+      acronym: "BTC",
+      url_image:"https://upload.wikimedia.org/wikipedia/commons/c/cf/Bitcoin.com_logo.png"
+      
+    },
+
+    {
+      description: "Ethereum",
+      acronym: "ETH",
+      url_image:"https://img2.gratispng.com/20180516/vgq/kisspng-ethereum-cryptocurrency-blockchain-logo-eos-io-crypto-5afc9ab9e70b61.4199610615265041219464.jpg"
+    }
+  ]
 )
 
-Coin.create!(
-  description: "Ethereum",
-  acronym: "ETH",
-  url_image:"https://img2.gratispng.com/20180516/vgq/kisspng-ethereum-cryptocurrency-blockchain-logo-eos-io-crypto-5afc9ab9e70b61.4199610615265041219464.jpg"
-)
+coins.each do |coin|
+  sleep(1)
+  Coin.find_or_create_by!(coin)
+end
 
-puts "Moedas criadas com sucesso!"
+spinner.success("Moedas criadas com sucesso!")
